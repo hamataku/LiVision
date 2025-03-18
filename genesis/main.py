829e230fcba898544808516ed27ae4c10966f7f0
hamataku <hamataku07130713@gmail.com>
2025-03-18 15:23:11 +0900

    eazydrone

--- genesis/main.py
+++ genesis/main.py
@@ -1,7 +1,6 @@
 import genesis as gs
 import numpy as np
 
-import genesis.ext.ros2.ros_core as ROS
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 
@@ -9,11 +8,11 @@ class Drone:
     def __init__(self):
         gs.init(backend=gs.gpu)
 
-        self.drone_pos = [0.0, 0.0, 1.0]
+        self.drone_pos = [0.0, 0.0, 0.1]
         self.drone_vel = [0.0, 0.0, 0.0]
 
         self.scene = gs.Scene(
-            show_viewer = True,
+            show_viewer = False,
             viewer_options = gs.options.ViewerOptions(
                 res           = (1280, 960),
                 camera_pos    = (2.0, 0.0, 2.0),
@@ -22,12 +21,13 @@ class Drone:
                 max_FPS       = 60,
             ),
             vis_options = gs.options.VisOptions(
-                show_world_frame = True,
+                show_world_frame = False,
                 world_frame_size = 1.0,
                 show_link_frame  = False,
                 show_cameras     = False,
                 plane_reflection = False,
                 ambient_light    = (0.1, 0.1, 0.1),
+                shadow = False,
             ),
             sim_options = gs.options.SimOptions(
                 dt=0.01
@@ -43,6 +43,7 @@ class Drone:
             gs.morphs.EasyDrone(
                 file = './drone.urdf',
                 pos  = self.drone_pos,
+                fixed = True
             )
         )
 
@@ -72,31 +73,33 @@ class Drone:
 
         self.scene.build()
     
-    def ros_init(self):
-        self.odom_pub = ROS.ros_node.create_publisher(Odometry, 'odom', 10)
+    # def ros_init(self):
+    #     self.odom_pub = ROS.ros_node.create_publisher(Odometry, 'odom', 10)
     
-    def ros_publish(self, count):
-        if count % 10 == 0:
-            odom_msg = Odometry()
-            odom_msg.header.stamp = ROS.ros_node.get_clock().now().to_msg()
-            odom_msg.header.frame_id = 'map'
-            odom_msg.child_frame_id = 'drone0'
-            odom_msg.pose.pose.position.x = self.drone_pos[0]
-            odom_msg.pose.pose.position.y = self.drone_pos[1]
-            odom_msg.pose.pose.position.z = self.drone_pos[2]
-            odom_msg.pose.pose.orientation.x = 0.0
-            odom_msg.pose.pose.orientation.y = 0.0
-            odom_msg.pose.pose.orientation.z = 0.0
-            odom_msg.pose.pose.orientation.w = 1.0
-            self.odom_pub.publish(odom_msg)
+    # def ros_publish(self, count):
+    #     if count % 10 == 0:
+    #         print(f"Drone position: {self.drone_pos}")
+            # odom_msg = Odometry()
+            # odom_msg.header.stamp = ROS.ros_node.get_clock().now().to_msg()
+            # odom_msg.header.frame_id = 'map'
+            # odom_msg.child_frame_id = 'drone0'
+            # odom_msg.pose.pose.position.x = self.drone_pos[0]
+            # odom_msg.pose.pose.position.y = self.drone_pos[1]
+            # odom_msg.pose.pose.position.z = self.drone_pos[2]
+            # odom_msg.pose.pose.orientation.x = 0.0
+            # odom_msg.pose.pose.orientation.y = 0.0
+            # odom_msg.pose.pose.orientation.z = 0.0
+            # odom_msg.pose.pose.orientation.w = 1.0
+            # self.odom_pub.publish(odom_msg)
         
     def run(self):
-        self.ros_init()
-
         for i in range(100000):
-            self.drone_vel = [0.1, 0.1, 0.0]
             self.scene.step()
-            self.ros_publish(i)
+            self.drone_vel = [0.1, 0.1, 0.0]
+            self.drone.set_velocity(self.drone_vel)
+            self.drone.update()
+            self.drone.render_sensors()
+            # self.ros_publish(i)
 
 if __name__ == '__main__':
     drone = Drone()
