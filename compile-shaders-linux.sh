3114b505ba1ac8685ef9e7cda8ca3c8d1682fec9
20hamataku <hamazaki-taku@g.ecc.u-tokyo.ac.jp>
2025-03-24 00:27:01 +0900

    delete super build

--- compile-shaders-linux.sh
+++ compile-shaders-linux.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
-# compile shaders
-
 mkdir -p shader/build
 
+SHADERC_PATH="./generated/bin/shaderc"
+
 echo "Compiling vertex shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/v_simple.sc -o shader/build/v_simple.bin \
 --platform linux --type vertex --verbose -i ./ -p spirv
 
 echo "Compiling fragment shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/f_simple.sc -o shader/build/f_simple.bin \
 --platform linux --type fragment --verbose -i ./ -p spirv
 
 echo "Compiling compute shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/compute_ray_cast.sc -o shader/build/compute_ray_cast.bin \
 --platform linux --type compute --verbose -i ./ -p spirv
--- compile-shaders-macos.sh
+++ compile-shaders-macos.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
-# compile shaders
-
 mkdir -p shader/build
 
+SHADERC_PATH="./generated/bin/shaderc"
+
 echo "Compiling vertex shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/v_simple.sc -o shader/build/v_simple.bin \
 --platform osx --type vertex --verbose -i ./ -p metal
 
 echo "Compiling fragment shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/f_simple.sc -o shader/build/f_simple.bin \
 --platform osx --type fragment --verbose -i ./ -p metal
 
 echo "Compiling compute shader"
-./third-party/build/bin/shaderc \
+$SHADERC_PATH \
 -f shader/compute_ray_cast.sc -o shader/build/compute_ray_cast.bin \
 --platform osx --type compute --verbose -i ./ -p metal
