12a22a7ca4068c9c91c2f34ebdb967fd7d8a5ab7
hamataku <hamataku07130713@gmail.com>
2025-10-01 13:52:28 +0900

    IMU fix

--- include/FastLS/sim/IMUObserver.hpp
+++ include/FastLS/sim/IMUObserver.hpp
@@ -18,9 +18,9 @@ class IMUObserver {
   explicit IMUObserver(ObjectBase* object, double acc_cov, double gyr_cov)
       : object_(object) {
     acc_dist_ = std::normal_distribution<>(
-        0.0F, std::sqrt(acc_cov * settings::common_dt));
+        0.0, std::sqrt(acc_cov / settings::common_dt));
     gyr_dist_ = std::normal_distribution<>(
-        0.0F, std::sqrt(gyr_cov * settings::common_dt));
+        0.0, std::sqrt(gyr_cov / settings::common_dt));
   }
 
   void Update() {
@@ -35,7 +35,7 @@ class IMUObserver {
       init_ = true;
       prev_quat_g_ = cur_quat_g;
       prev_pos_g_ = cur_pos_g;
-      acc_l_ = glm::inverse(cur_quat_g) * glm::dvec3(0.0, 0.0, gravity_);
+      acc_l_ = glm::inverse(cur_quat_g) * glm::dvec3(0.0, 0.0, kGravity);
       return;
     }
 
@@ -45,25 +45,32 @@ class IMUObserver {
     prev_vel_g_ = vel_g;
 
     // Add gravity in world frame
-    acc_g += glm::dvec3(0.0F, 0.0F, gravity_);
+    acc_g += glm::dvec3(0.0, 0.0, kGravity);
 
     // Transform to local frame
-    acc_l_ = glm::inverse(cur_quat_g) * acc_g;
-    acc_l_ +=
-        glm::dvec3(acc_dist_(engine_), acc_dist_(engine_), acc_dist_(engine_));
+    glm::dvec3 acc_l_new = glm::inverse(cur_quat_g) * acc_g;
+    glm::dvec3 acc_l_diff = acc_l_new - acc_l_;
+
+    if (glm::length(acc_l_diff) > kAccAbnormalityThreshold) {
+      // 異常値検出したら、前の値を使う
+      acc_l_diff = glm::dvec3(0.0);
+    }
+
+    acc_l_ += acc_l_diff + glm::dvec3(acc_dist_(engine_), acc_dist_(engine_),
+                                      acc_dist_(engine_));
 
     // Calculate angular velocity
     glm::dquat diff_quat_g =
         glm::normalize(cur_quat_g * glm::inverse(prev_quat_g_));
     if (diff_quat_g.w < 0) diff_quat_g = -diff_quat_g;  // 符号の一貫性を保つ
     glm::dvec3 angular_vel_g;
-    double sin_half_angle = std::sqrt(1.0F - (diff_quat_g.w * diff_quat_g.w));
+    double sin_half_angle = std::sqrt(1.0 - (diff_quat_g.w * diff_quat_g.w));
     // 修正後
-    if (sin_half_angle > 1e-6F) {
+    if (sin_half_angle > 1e-6) {
       glm::dvec3 axis =
           glm::dvec3(diff_quat_g.x, diff_quat_g.y, diff_quat_g.z) /
           sin_half_angle;
-      double angle = 2.0F * std::atan2(sin_half_angle, diff_quat_g.w);
+      double angle = 2.0 * std::atan2(sin_half_angle, diff_quat_g.w);
       angular_vel_g = (angle / settings::common_dt) * axis;
     } else {
       // 小角度近似: angle ≈ 2 * sin(angle/2), axis*sin(angle/2) ≈ vec_part
@@ -85,15 +92,17 @@ class IMUObserver {
   glm::dvec3 GetAngularVel() const { return angular_vel_l_; }
 
  private:
-  const double gravity_ = 9.81F;
+  static constexpr double kGravity = 9.81;
+  static constexpr double kAccAbnormalityThreshold = 1.0;  // (m/s^2)
+
   bool init_ = false;
 
   glm::dquat prev_quat_g_;
   glm::dvec3 prev_pos_g_;
-  glm::dvec3 prev_vel_g_ = glm::dvec3(0.0F);
+  glm::dvec3 prev_vel_g_ = glm::dvec3(0.0);
 
-  glm::dvec3 acc_l_ = glm::dvec3(0.0F);
-  glm::dvec3 angular_vel_l_ = glm::dvec3(0.0F);
+  glm::dvec3 acc_l_ = glm::dvec3(0.0);
+  glm::dvec3 angular_vel_l_ = glm::dvec3(0.0);
 
   ObjectBase* object_ = nullptr;
 
