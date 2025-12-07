700d5e93c22ab6695003b563eab7aa7929041684
hamataku <hamataku07130713@gmail.com>
2025-12-07 12:30:03 +0900

    lidar_angle_offset

--- include/FastLS/object/DroneLIdarUp.hpp
+++ include/FastLS/object/DroneLIdarUp.hpp
@@ -7,13 +7,14 @@ namespace fastls {
 class DroneLidarUp : public Container {
  public:
   void InitImpl() final {
-    lidar_.SetPos(glm::dvec3(0, 0, 0.25));
+    lidar_.SetPos(glm::dvec3(0, 0, 0.25)).SetRadRotation(lidar_angle_offset_);
     AddObject(&lidar_);
 
     AddObject(&body_);
   }
 
   Lidar lidar_;
+  glm::dvec3 lidar_angle_offset_{0, 0, 0};
 
  private:
   DroneBody body_;
--- include/FastLS/object/DroneLidarDown.hpp
+++ include/FastLS/object/DroneLidarDown.hpp
@@ -7,13 +7,14 @@ namespace fastls {
 class DroneLidarDown : public Container {
  public:
   void InitImpl() final {
-    lidar_.SetPos(glm::dvec3(0, 0, 0.2)).SetDegRotation(glm::dvec3(-180, 0, 0));
+    lidar_.SetPos(glm::dvec3(0, 0, 0.2)).SetRadRotation(lidar_angle_offset_);
     AddObject(&lidar_);
 
     AddObject(&body_);
   }
 
   Lidar lidar_;
+  glm::dvec3 lidar_angle_offset_{0, M_PI, 0};
 
  private:
   DroneBody body_;
