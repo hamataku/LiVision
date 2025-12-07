138f87a95864e0c6128873a8df3d822abe0a5eab
hamataku <hamataku07130713@gmail.com>
2025-12-07 16:56:14 +0900

    lidar

--- include/FastLS/object/Lidar.hpp
+++ include/FastLS/object/Lidar.hpp
@@ -4,17 +4,19 @@
 #include "FastLS/object/Cylinder.hpp"
 #include "FastLS/sensor/LidarSensor.hpp"
 #include "FastLS/sim/LidarSim.hpp"
+#include "FastLS/utils.hpp"
 
 namespace fastls {
 
 class Lidar : public Container {
  public:
   void InitImpl() final {
-    body_.SetSize(glm::dvec3(0.2, 0.2, 0.2))
-        .SetPos(glm::dvec3(0.0, 0.0, 0))
-        .SetColor(utils::blue)
-        .SetLidarVisible(false);
+    body_.SetSize(glm::dvec3(0.06, 0.06, 0.03))
+        .SetPos(glm::dvec3(0.0, 0.0, 0.015))
+        .SetColor(utils::light_gray);
     AddObject(&body_);
+
+    lidar_sensor_.SetPos(glm::dvec3(0, 0, 0.04));
 
     if (lidar_enable_) {
       lidar_sim.RegisterLidar(&lidar_sensor_);
