08384e11f33e8582225be2c5b3e9730451fb4848
hamataku <hamataku07130713@gmail.com>
2025-12-31 14:25:44 +0900

    grid

---
+++ include/FastLS/object/Grid.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "FastLS/object/Cylinder.hpp"
+#include "FastLS/object/ObjectBase.hpp"
+
+namespace fastls {
+
+class Grid : public ObjectBase {
+ public:
+  void Draw(bgfx::ProgramHandle& program) final {
+    cylinder_.SetColor(color_);
+
+    // Draw grid lines along X axis
+    for (double x = pos_.x - (size_.x / 2); x <= pos_.x + (size_.x / 2);
+         x += resolution_) {
+      cylinder_.SetPos(glm::dvec3(x, pos_.y, pos_.z));
+      cylinder_.SetSize(glm::dvec3(0.05, 0.05, size_.y));
+      cylinder_.SetDegRotation(glm::dvec3(90.0, 0.0, 0.0));
+      cylinder_.UpdateMatrix();
+      cylinder_.Draw(program);
+    }
+    // Draw grid lines along Y axis
+    for (double y = pos_.y - (size_.y / 2); y <= pos_.y + (size_.y / 2);
+         y += resolution_) {
+      cylinder_.SetPos(glm::dvec3(pos_.x, y, pos_.z));
+      cylinder_.SetSize(glm::dvec3(0.05, 0.05, size_.x));
+      cylinder_.SetDegRotation(glm::dvec3(0.0, 90.0, 0.0));
+      cylinder_.UpdateMatrix();
+      cylinder_.Draw(program);
+    }
+  }
+
+  Grid& SetResolution(double resolution) {
+    resolution_ = resolution;
+    return *this;
+  }
+
+ private:
+  Cylinder cylinder_;
+  double resolution_ = 10.0;
+};
+}  // namespace fastls
